db5ae66bc86e22d42fcc9e4fc13edbb844397a49
OnceTrue <wooc08@daum.net>
2021-08-19 00:23:51 +0900

    0819

--- 0810.tf
+++ 0810.tf
@@ -21,29 +21,76 @@ resource "azurerm_subnet" "interanl" {
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes = ["150.0.1.0/24"]
 }
+resource "azurerm_public_ip" "wcpip" {
+  name = "${var.prefix}-Pip"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  allocation_method = "Dynamic"
+  idle_timeout_in_minutes = 30
+  tags = {
+    enviroment = "public-ip"
+  }
+}
 resource "azurerm_network_interface" "main" {
   name = "${var.prefix}-nic"
-  loaction = azurerm_resource_group.main.location
-  resourec_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   ip_configuration {
     name = "wooc"
     subnet_id = azurerm_subnet.interanl.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.wcpip.id
 
   }
 }
-
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_network_security_group" "wcnsg" {
+  name = "${var.prefix}-nsg"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    name = "ssh"
+    priority = 100
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+   }
+security_rule {
+    name = "port80"
+    priority = 110
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "80"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+}
+}
+resource "azurerm_network_interface_security_group_association" "wcnsg" {
+  network_interface_id = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.wcnsg.id
+} 
+resource "azurerm_virtual_machine" "wcvm" {
   name = "${var.prefix}-vm"
-  loaction = azurerm_resource_group.main.location
-  resourec_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size = "Standard_DS1_v2"
 
+storage_image_reference {
+  publisher = "Canonical"
+  offer = "UbuntuServer"
+  sku = "16.04-LTS"
+  version = "latest" 
+}
 storage_os_disk {
-  name = "${var.prefix}-disk"
+  name = "${var.prefix}-woocdisk"
   caching = "ReadWrite"
-  createoption = "FromImage"
+  create_option = "FromImage"
   managed_disk_type = "Standard_LRS"
 }
 os_profile {
@@ -54,6 +101,7 @@ os_profile {
 os_profile_linux_config {
   disable_password_authentication =false
 }
+
 tags = {
   enviroment = "wc-tf"
   }
